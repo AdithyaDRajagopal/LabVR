f8ef96ede967d6cafe84acc5637a6a854b8448b9
Adarsh S <adarshsuresh@cet.ac.in>
2021-05-16 05:47:48 +0530

    login-api successfull

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+public class UserData 
+{
+    public string user="student";
+    public string password;
+    public string email;
+}
+
+public class Response{
+    public string message;
+    public string token;
+}
 
 public class Login : MonoBehaviour
 {
@@ -18,22 +30,51 @@ public class Login : MonoBehaviour
         KeyInput.SetActive(false);
     }
 
-    public void login()
-    {
+    public void login(){
+
         string email = Email.text;
         string pass = Password.text;
-        if (email == "abc") // If user in database
-        {
-            if (pass == "123") // If password is the user's password
-            {
-                gameObject.SetActive(false);
-                KeyInput.SetActive(true);
-            }
-            else
-                print("Wrong password");
+        //API Call
+        StartCoroutine(loginAuth(email, pass));
+    }
+
+    public  IEnumerator loginAuth(string email, string pass)
+    {
+        var user = new UserData();
+        user.email = email;
+        user.password = pass;
+        Debug.Log(user);
+        string json = JsonUtility.ToJson(user);
+
+        var req = new UnityWebRequest("https://vrlabserver.herokuapp.com/api/login", "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+
+        //Send the request then wait here until it returns
+        yield return req.SendWebRequest();
+
+        if (req.isNetworkError){
+            Debug.Log("Error While Sending: " + req.error);
         }
-        else
-            print("User Invalid");
+        else{
+            var res=new Response();
+            JsonUtility.FromJsonOverwrite(req.downloadHandler.text, res);
+            Debug.Log("Received Token: " + res.token);
+        }
+        // if (email == "abc") // If user in database
+        // {
+        //     if (pass == "123") // If password is the user's password
+        //     {
+        //         gameObject.SetActive(false);
+        //         KeyInput.SetActive(true);
+        //     }
+        //     else
+        //         print("Wrong password");
+        // }
+        // else
+        //     print("User Invalid");
     }
 
     public void StartExpt()
